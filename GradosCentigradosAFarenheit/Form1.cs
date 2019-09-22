5284353c77d12b292add1c6da732e4c9eb4dac81
Brahian Julian Mejia Perez <brayantqm123@gmail.com>
2019-09-22 11:34:52 -0500

    Uso de clases y métodos es C Sharp.

---
+++ GradosCentigradosAFarenheit/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradosCentigradosAFarenheit
+{
+    public partial class Form1 : Form
+    {
+        Temperatura temperatura= new Temperatura();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnconvertir_Click(object sender, EventArgs e)
+        {
+            temperatura.setGrados(double.Parse(txtcentigrados.Text));
+
+            lblresultado.Text = "Resultado: " + temperatura.calculo().ToString();
+            lblresultado.Visible = true;
+        }
+    }
+}
---
+++ GradosCentigradosAFarenheit/Temperatura.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradosCentigradosAFarenheit
+{
+    class Temperatura
+    {
+        private double grados;
+
+        public void setGrados(double valor)
+        {
+            this.grados = valor;
+        }
+
+        public double getGrados()
+        {
+            return this.grados;
+        }
+
+        public double calculo()
+        {
+            return ((this.getGrados() * 9/5) + (32));
+        }
+
+    }
+}
